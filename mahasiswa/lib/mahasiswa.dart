031b8a2c66cc1a7215fcfc59c4c45c1d3f69a286
maryatulkippiyah <maryatulkippiyah01@gmail.com>
2021-04-19 14:49:03 +0700

    ini adalah commit untuk UTS

---
+++ mahasiswa/lib/mahasiswa.dart
@@ -0,0 +1,42 @@
+import 'package:mahasiswa/ketuahima.dart';
+
+class Mahasiswa extends KetuaHima {
+  String _nrp, _jurusan;
+  int _nilai_jurusan;
+
+  set nilai_jurusan(int nilai_jurusan) {
+    _nilai_jurusan = nilai_jurusan;
+  }
+
+  set nrp(String nrp) {
+    _nrp = nrp;
+  }
+
+  String get_jurusan(prodi) {
+    if (prodi >= 1) {
+      _jurusan = 'Teknik Informatika';
+    } else if (prodi >= 2) {
+      _jurusan = 'Teknik Listrik';
+    } else if (prodi >= 3) {
+      _jurusan = 'Teknik Sipil';
+    } else if (prodi >= 4) {
+      _jurusan = 'Administrasi Niaga';
+    } else if (prodi >= 5) {
+      _jurusan = 'Teknik Mesin';
+    } else if (prodi >= 6) {
+      _jurusan = 'Teknik perkapalan';
+    } else if (prodi >= 7) {
+      _jurusan = 'Maritim';
+    }
+    return _jurusan;
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'nrp': _nrp,
+      'nama': nama,
+      'nilai_jurusan': _nilai_jurusan,
+      'jurusan': get_jurusan(_nilai_jurusan),
+    };
+  }
+}
